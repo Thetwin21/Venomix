b359247b764977248b52d499a37b3ae3597a2f38
sylvester ogodo <sylvesterogodo@gmail.com>
2023-05-30 19:48:23 +0100

    new changes

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import CreateProfile from './Components/ProfileSec/CreateProfile';
 
 function App() {
   return (
-    <div className="">
+    <div className="w-full overflow-hidden">
       
       <Routes>
         <Route path='/' element={<LandingPg />} />

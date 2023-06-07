4cdda73d333e03716b277c547997419f5349dbdb
Daniel Eni <enidaniel15@gmail.com>
2023-06-07 03:29:41 -1100

    Dashboard adjusted#

--- src/Components/Dashboard/DashboardPanel.js
+++ src/Components/Dashboard/DashboardPanel.js
@@ -9,7 +9,7 @@ const DashboardPanel = () => {
                 <button className='rounded-[30px] bg-[#F5167E] font-light text-[#fff] px-10 py-1 text-[16px] cursor-pointer'>Create</button>
             </div>
             <div className='flex md:flex-row md:space-x-3 space-x-0 md:space-y-0 space-y-3 flex-col justify-between items-center'>
-                <div className='md:w-auto w-80 flex flex-col justify-center p-10 space-y-2 items-center rounded-md border-solid border-2 border-inherit'>
+                <div className='md:w-auto w-80 text-center flex flex-col justify-center p-10 space-y-2 items-center rounded-md border-solid border-2 border-inherit'>
                     <h1 className='text-black text-2xl font-semibold'>$0</h1>
                     <p>This is your total revenue generated</p>
                     <button className='rounded-[30px] bg-[#F5167E] font-light text-[#fff] px-10 py-1 text-[16px] cursor-pointer'>Learn More</button>
